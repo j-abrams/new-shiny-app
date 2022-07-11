be7993f15b6a9b3decac2e8b884e1e1c20520a6c
j-abrams <james.abrams@hmcm.co.uk>
2022-07-11 16:19:08 +0100

    sample shiny app

--- server.R
+++ server.R
@@ -2,13 +2,12 @@
 
 
 
-server <- shinyServer(function(input, output, session) {
-  
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
   
   output$plot1 <- renderPlot({
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
-  
-})
-
+}
