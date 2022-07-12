c66d6232954ac0bfe281053c3afb43687a834761
j-abrams <james.abrams@hmcm.co.uk>
2022-07-12 11:50:33 +0100

    added reactivity to slider and checkbox inputs

--- server.R
+++ server.R
@@ -1,30 +1,53 @@
 
 
-server <- function(input, output) {
+if (interactive()) {
   
-
-  vals <- reactiveValues()
-  observe({
-    vals$jdata <- jdata_new %>%
-      filter(Actuals %in% input$checkbox1)
+  server <- function(input, output, session) {
     
-    print(min(vals$jdata$Date))
-  })
   
-  
-  output$text = renderPrint(input$slider1)
-  
-  output$table <- renderTable({
-    jdata_new %>%
-      # Choose whether to display either actuals or projections, or both
-      filter(Actuals %in% input$checkbox1) %>%
-      select(-Actuals) %>%
-      # Use floor date to filter on Date
-      filter(Date >= floor_date(input$slider1[1], unit = "months") & 
-             Date <= floor_date(input$slider1[2], unit = "months")) %>%
-      mutate(Date = format(Date, "%b-%y"))
-  })
-  
+    vals <- reactiveValues()
+    observe({
+      vals$jdata <- jdata_new %>%
+        filter(Actuals %in% input$checkbox1)
+      
+      print(min(vals$jdata$Date))
+    })
+    
+    
+    observeEvent(input$checkbox1, {
+      
+      require(length(input$checkbox1) > 0)
+      
+      vals$jdata <- jdata_new %>%
+        filter(Actuals %in% input$checkbox1)
+      
+      updateSliderInput(session, "slider1", 
+                        label = "Date:",
+                        min = as.Date(min(vals$jdata$Date), "%b-%y"),
+                        max = as.Date(max(vals$jdata$Date), "%b-%y"),
+                        value = c(as.Date(min(vals$jdata$Date), "%b-%y"),
+                                  as.Date(max(vals$jdata$Date), "%b-%y")),
+                        timeFormat = "%b-%y")
+    }, ignoreInit = TRUE )
+    
+    
+    output$text = renderPrint(floor_date(input$slider1, unit = "months"))
+    
+    output$table <- renderTable({
+      jdata_new %>%
+        # Choose whether to display either actuals or projections, or both
+        filter(Actuals %in% input$checkbox1) %>%
+        select(-Actuals) %>%
+        # Use floor date to filter on Date
+        filter(Date >= floor_date(input$slider1[1], unit = "months") & 
+               Date <= floor_date(input$slider1[2], unit = "months")) %>%
+        mutate(Date = format(Date, "%b-%y"))
+    })
+    
+    
+  }
   
 }
+
+
 
